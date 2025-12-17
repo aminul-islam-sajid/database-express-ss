0a82b42f2901dfb760041b3c7d8358b95f295119
aminul-islam-sajid <mdaminulislamsajid@gmail.com>
2025-12-17 23:07:45 +0600

    server is running

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
-import express from "express"
+import express, { Request, Response } from "express"
 
 const app = express()
+app.use(express.json())
+
+app.post("/users", (req: Request, res: Response)=>{
+    const body = req.body
+    console.log(body);
+})
+
+app.get('/', (req: Request, res: Response)=>{
+    res.status(200).json({
+        message: "this is the root route",
+        path: req.path
+    })
+})
 
 app.listen(5000,()=>{
     console.log("server is running in 5000");
