4b88722f0fbff463d70069179cfd67abc9298f2f
aminul-islam-sajid <mdaminulislamsajid@gmail.com>
2025-12-18 15:56:30 +0600

    database connected

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,27 @@
 import express, { Request, Response } from "express"
+import {Pool} from "pg"
 
 const app = express()
 app.use(express.json())
+
+const pool = new Pool({
+connectionString: 'postgresql://neondb_owner:npg_sXIVRtT35HFZ@ep-blue-salad-a8y71sfu-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require'
+})
+
+const initDb = async ()=>{
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(250) NOT NULL,
+        password TEXT NOT NULL ,
+        age INT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `)
+        console.log("database connected");
+}
+initDb()
 
 app.post("/users", (req: Request, res: Response)=>{
     const body = req.body
